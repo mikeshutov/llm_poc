0f9f850752787c8c9348967fd548c2bec712190c
Mike Shutov <mikeshutov@gmail.com>
2026-07-01 07:52:51 -0400

    use 5.4 as the min

--- common/model_constants.py
+++ common/model_constants.py
@@ -1,5 +1,5 @@
 import os
 
-LLM_MODEL = os.getenv("LLM_MODEL", "gpt-5.1")
+LLM_MODEL = os.getenv("LLM_MODEL", "gpt-5.4")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "text-embedding-3-small")
 CHUNK_ENCODING = "cl100k_base"
