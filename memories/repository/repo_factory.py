140152556f0f9f6e4c9c57f9d69341ddb95287ec
Mike Shutov <mikeshutov@gmail.com>
2026-07-30 08:09:37 -0400

    cleanup

--- memories/repository/repo_factory.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-from memories.repository.memory_repository import MemoryRepository
-
-
-@st.cache_resource
-def get_memory_repo() -> MemoryRepository:
-    return MemoryRepository()
